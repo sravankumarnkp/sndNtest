e36b06c99a0b0ff89012bd1b50ed0a44c323bdc0
sravankumarnkp <sravankumarnkp@gmail.com>
2021-10-06 13:26:21 +1100

    specflow (feature file ) and specflow steps (step defination/hook-up file) created

---
+++ sndNunitest/SpecFlow/TMFlowFeature.feature
@@ -0,0 +1,27 @@
+﻿Feature: TMFlowFeature
+	As a turnup Portal Admin \
+	I would like to create , edit ,delete  time and material records
+	so that i can manage employees time and meterial sucessfully
+
+@tmtest @regression
+Scenario: Create Time and Material with valid details
+	Given I Logged into turnup portal sucessfully
+	And I nagivate to Time and material page
+	When I create time and material record.
+	Then Thre record should be created sucessfully
+
+@tmtest
+Scenario: Edit Time And Material With Valid Details
+Given I Logged into turnup portal sucessfully
+And I nagivate to Time and material page
+When I Edit the time and material record
+Then There record should be edited sucessfully
+
+@tmtest
+Scenario: delete Time and Material record with valid details
+Given I Logged into turnuo portal sucessfully
+And I nagivate to Time and material page
+When I delete the time and marerial record
+Then there record should be deleted sucessfully.
+
+
---
+++ sndNunitest/StepDefination/TMFlowFeatureSteps.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace sndNunitest.StepDefination
+{
+    [Binding]
+    public class TMFlowFeatureSteps
+    {
+        [Given(@"I Logged into turnup portal sucessfully")]
+        public void GivenILoggedIntoTurnupPortalSucessfully()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I nagivate to Time and material page")]
+        public void GivenINagivateToTimeAndMaterialPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I Logged into turnuo portal sucessfully")]
+        public void GivenILoggedIntoTurnuoPortalSucessfully()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I create time and material record\.")]
+        public void WhenICreateTimeAndMaterialRecord_()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I Edit the time and material record")]
+        public void WhenIEditTheTimeAndMaterialRecord()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I delete the time and marerial record")]
+        public void WhenIDeleteTheTimeAndMarerialRecord()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Thre record should be created sucessfully")]
+        public void ThenThreRecordShouldBeCreatedSucessfully()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"There record should be edited sucessfully")]
+        public void ThenThereRecordShouldBeEditedSucessfully()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"there record should be deleted sucessfully\.")]
+        public void ThenThereRecordShouldBeDeletedSucessfully_()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
